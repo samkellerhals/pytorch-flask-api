da128b227b459c2148b8a4fd1309948129db4aed
samkellerhals <kellerhalssamuel@gmail.com>
2020-03-20 01:16:57 +0100

    process images from S3

--- torch_api/views.py
+++ torch_api/views.py
@@ -3,6 +3,11 @@ from flask import request, make_response
 from flask_restful import Resource 
 from torch_api.model import get_prediction
 import werkzeug
+import requests
+from PIL import Image
+import wget
+import os
+import glob
 
 class Home(Resource):
     def get(self):
@@ -10,16 +15,26 @@ class Home(Resource):
 
 class PredictImage(Resource):
     def post(self):
-        files = request.files # get all files
-        images = files.getlist('image') # get list of images
-        
+        image_urls = request.get_json(force=True)
+        img_folder = 'torch_api/img/'
         predictions = []
+        
+        for i in range(len(image_urls)):
+            image_name = image_urls[i]
+            wget.download(image_urls[i], out=img_folder + 'image' + str(i) + '.png')
 
-        for i in range(len(images)): # make predictions for each image
-            img_bytes = images[i].read()
+        for i in range(len(os.listdir(img_folder))): # make predictions for each image
+            f = open(img_folder + 'image' + str(i) + '.png', 'rb')
+            img_bytes = f.read()
+            f.close()
             preds = get_prediction(img_bytes)
             preds_dict = {'image' : str(i), 'class' : preds[1]}
             predictions.append(preds_dict)
+
+        # clean-up (delete all images in directory)
+        folder = glob.glob(img_folder + '*')
+        for f in folder:
+            os.remove(f)
 
         return predictions, 200
 
